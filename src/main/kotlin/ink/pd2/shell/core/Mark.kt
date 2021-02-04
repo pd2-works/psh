c11cc741b01b4415faf166f8022f712c243357ba
Maxel Black <maxelblack@outlook.com>
2021-02-04 14:43:27 +0800

    移除Mark中毫无用处的check方法

--- src/main/kotlin/ink/pd2/shell/core/Mark.kt
+++ src/main/kotlin/ink/pd2/shell/core/Mark.kt
@@ -8,37 +8,6 @@ object Mark {
 	}
 
 	/**
-	 * ## checkSign() | 检查标记
-	 *
-	 * 用于找出字符串中存在的指定记号(形如字符串"&sign:xxx&", sign即为记号)
-	 *
-	 * @param s 在其中查找的字符串
-	 * @param sign 要查找的记号
-	 *
-	 * @return 被指定记号标记的所有子字符串
-	 *
-	 * @author Maxel Black (maxelblack@outlook.com)
-	 */
-
-	fun checkSign(sign: String, s: String): Array<String> {
-		val strings = ArrayList<String>(listOf())
-		val l = s.length
-		var i = 0
-		while (i < l){
-			val i1 = s.indexOf('&', i)
-			if (i1 == -1 || (i1 != 0 && s[i1 - 1] != '\\')) break
-			val i2 = s.indexOf('&', i1 + 1)
-			if (i2 == -1 || s[i2-1] != '\\') break
-			if (i1 != i2) {
-				val ss = s.substring(i1 + 1, i2 - 1).split(':')
-				if (ss.size == 2 && ss[0] == sign) strings.add(ss[1])
-			}
-			i = i2 + 1
-		}
-		return strings.toArray() as Array<String>
-	}
-
-	/**
 	 * ## update() 02 | 按标记规则更新
 	 *
 	 * 用于按照指定的标记规则更新字符串(该标记规则需已注册, 参见 `regMarkProvider()` )
@@ -56,40 +25,6 @@ object Mark {
 	fun updateSign(sign: String, s: String) {
 		TODO()
 	}
-
-	/**
-	 * ## checkAllSign() | 检查所有标记
-	 *
-	 * 用于找出字符串中存在的所有已注册的记号(形如字符串"&sign:xxx&", sign即为记号)
-	 *
-	 * @param s 在其中查找的字符串
-	 *
-	 * @return 被标记的所有字符串和标记它的记号(参见 `MarkString` )
-	 *
-	 * @see ink.pd2.shell.core.Mark.MarkString
-	 *
-	 * @author Maxel Black (maxelblack@outlook.com)
-	 */
-
-	fun checkAllSign(s: String): Array<MarkString> {
-		val markStrings = ArrayList<MarkString>(listOf())
-		val l = s.length
-		var i = 0
-		while (i < l){
-			val i1 = s.indexOf('&', i)
-			if (i1 == -1 || (i1 != 0 && s[i1 - 1] != '\\')) break
-			val i2 = s.indexOf('&', i1 + 1)
-			if (i2 == -1 || s[i2-1] != '\\') break
-			if (i1 != i2) {
-				val ss = s.substring(i1 + 1, i2 - 1).split(':')
-				if (ss.size == 2 && marks.containsKey(ss[0]))
-					markStrings.add(MarkString(ss[0], ss[1]))
-			}
-			i = i2 + 1
-		}
-		return markStrings.toArray() as Array<MarkString>
-	}
-	class MarkString(val sign: String, val s: String)
 
 	/**
 	 * ## update() 01 | 按标记规则更新
